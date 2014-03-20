a0b4f626b959e4f38e47c58e54b90b81d97cb27e
RW <richard@panchesco.com>
2014-03-20 16:40:36 -0700

    more revisions to .gitignore

---
+++ config/config-sample.php
@@ -0,0 +1,11 @@
+<?php
+/** 
+ * Current Instagram client settings.
+ * More info: http://instagram.com/developer/
+ */
+ 
+$config['client_id']		= '';
+$config['client_secret']	= '';
+$config['website_url']		= '';
+$config['redirect_uri'] 	= '';
+$config['access_token']		= '';
