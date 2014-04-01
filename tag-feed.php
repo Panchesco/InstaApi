8526cc1ce1b0c3972f994ddee487f6a7eef413e6
RW <richard@panchesco.com>
2014-04-01 10:22:04 -0700

    Using MaxCDN hosted Bootstrap for now

--- tag-feed.php
+++ tag-feed.php
@@ -12,15 +12,14 @@
     <meta charset="utf-8">
     <title>Tag Feed for #<?php echo $feed->tag ;?></title>
     
-    
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="/bootstrap/dist/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
 </head>
 <body>
